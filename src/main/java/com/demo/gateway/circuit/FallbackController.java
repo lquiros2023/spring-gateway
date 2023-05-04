14ef55907f66491d42525246ed60b19db8839c03
Luis Quiros <luis.quiros@pfcti.com>
2023-05-04 15:37:10 -0600

    Cambios 4 de mayo

---
+++ src/main/java/com/demo/gateway/circuit/FallbackController.java
@@ -0,0 +1,24 @@
+package com.demo.gateway.circuit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+/* Retorno con error 200
+    @GetMapping("/service-fallback")
+    public Mono<String> fallbackService() {
+        return Mono.just("Por el momento nos encontramos con problemas técnicos, por favor intente en unos minutos.");
+    }
+
+ */
+
+//Retorno con error 504
+    @GetMapping("/service-fallback")
+    public ResponseEntity<String> fallbackService() {
+        return new ResponseEntity<>("Por el momento nos encontramos con problemas técnicos, por favor intente en unos minutos.", HttpStatus.GATEWAY_TIMEOUT);
+    }
+}
--- src/main/java/com/demo/gateway/routes/RoutesConfig.java
+++ src/main/java/com/demo/gateway/routes/RoutesConfig.java
@@ -1,11 +1,16 @@
 package com.demo.gateway.routes;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 public class RoutesConfig {
@@ -15,7 +20,10 @@ public class RoutesConfig {
     @Value("${uri.api.cuentas}")
     private String cuentasUri;
 
-    @Bean
+    @Value("${uri.api.clientes2}")
+    private String clientesUri2;
+
+  /*version 1  @Bean
 
     public RouteLocator rutasDeClientes(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -34,6 +42,46 @@ public class RoutesConfig {
                 )
                 .build();
     }
+
+
+
+    //Versión identifica el canal por medio del Header
+    @Bean
+    public RouteLocator rutasDeClientes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("all_clientes1", r -> r.path("/promerica/todos-los-clientes")
+                        .and().predicate(serverWebExchange -> {
+                            HttpHeaders headers = serverWebExchange.getRequest().getHeaders();
+                            String canal = headers.get("CANAL").get(0);
+                            return canal.equals("WEB");
+                        })
+                        .filters(f -> f.rewritePath("/promerica/todos-los-clientes", "/v1/api/cliente/all")
+                                .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
+                                .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
+                        .uri(clientesUri)
+                )
+                .route("all_clientes2", r -> r.path("/promerica/todos-los-clientes")
+                        .and().predicate(serverWebExchange -> {
+                            HttpHeaders headers = serverWebExchange.getRequest().getHeaders();
+                            String canal = headers.get("CANAL").get(0);
+                            return canal.equals("MOBILE");
+                        })
+                        .filters(f -> f.rewritePath("/promerica/todos-los-clientes", "/v1/api/cliente/all")
+                                .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
+                                .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
+                        .uri(clientesUri2)
+                )
+                .route("cliente_id", r -> r.path("/promerica/cliente/{id}")
+                        .and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT)
+                        .filters(f -> f.rewritePath("/promerica/", "/v2/api/")
+                                .addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY-SOLITUD")
+                                .addResponseHeader("X-GATEYWAY-RESPONSE-HEADER", "GATEWAY_RESPONSE"))
+                        .uri(clientesUri)
+                )
+                .build();
+    }
+
+
     @Bean
     public RouteLocator rutasDeCuentas(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -43,7 +91,11 @@ public class RoutesConfig {
                         ).uri(cuentasUri))
                 .build();
     }
-
+   */
+  @Bean
+  public KeyResolver userKeyResolver() {
+      return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
+  }
 
 
 }
